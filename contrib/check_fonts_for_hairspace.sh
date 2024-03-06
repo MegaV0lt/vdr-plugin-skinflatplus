21c194eba13a4f6260ce569c73c01dc2c8e313a3
MegaV0lt <M_V0lt@mail.ru>
2024-03-06 17:37:27 +0100

    Add script 'check_fonts_for_hairspace.sh' to list fonts with 'HairSpace'

---
+++ contrib/check_fonts_for_hairspace.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# List fonts that have the char 'Hairspace'
+
+# /usr/share/fonts/truetype/freefont/FreeMono.ttf: FreeMono:style=Standard,нормален,normal,obyčejné,µεσαία,Regular,Normaali,Normál,Normale,Standaard,Normalny,Обычный,Normálne,menengah,прямій,navadno,vidējs,normalusis,thường,Arrunta,सामान्य
+echo -e 'Schriften, die das Zeichen "HairPace" enthalten:\n'
+mapfile -t < <(fc-list ":charset=0x200A" | sort -u)
+for font in "${MAPFILE[@]}" ; do
+  file="${font%%: *}" ; name=""${font#* }
+  echo "Font: ${name%%,*} (${file##*/})"  # FreeMono:style=standard (FreeMono.ttf)
+done
