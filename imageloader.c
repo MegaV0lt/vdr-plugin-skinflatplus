60a17e5c24d326f5b66ffe5fa84aa769dc0f2673
MegaV0lt <M_V0lt@mail.ru>
2024-12-08 09:37:29 +0100

    Use 'std::replace()'

--- imageloader.c
+++ imageloader.c
@@ -35,11 +35,7 @@ cImage* cImageLoader::LoadLogo(const char *logo, int width, int height) {
             ToLowerCase(LogoLower);  // Convert to lowercase (A-Z)
             File = cString::sprintf("%s/%s.%s", *Config.LogoPath, LogoLower.c_str(), *m_LogoExtension);
         } else if (i == 2) {            // Third try. Search for lowercase logo with '~' for path '/'
-            const std::size_t LogoLowerLength {LogoLower.length()};
-            for (std::size_t i {0}; i < LogoLowerLength; ++i) {
-                if (LogoLower[i] == '/')
-                    LogoLower[i] = '~';
-            }
+            std::replace(LogoLower.begin(), LogoLower.end(), '/', '~');
             File = cString::sprintf("%s/%s.%s", *Config.LogoPath, LogoLower.c_str(), *m_LogoExtension);
         }
 #ifdef DEBUGIMAGELOADTIME
