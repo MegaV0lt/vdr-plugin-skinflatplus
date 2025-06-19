1f17072d9c5bb1103db76bda7febb83eeb3bfcb7
MegaV0lt <M_V0lt@mail.ru>
2025-06-19 22:28:54 +0200

    Reformat

--- baserender.h
+++ baserender.h
@@ -34,180 +34,180 @@ struct sDecorBorder {
 
 class cFlatBaseRender {
  public:
-        cImageLoader ImgLoader;
+    cImageLoader ImgLoader;
 
-        cFlatBaseRender();
-        ~cFlatBaseRender();
+    cFlatBaseRender();
+    ~cFlatBaseRender();
 
-        void CreateFullOsd();
-        void CreateOsd(int Left, int Top, int Width, int Height);
+    void CreateFullOsd();
+    void CreateOsd(int Left, int Top, int Width, int Height);
 
-        void TopBarCreate();
-        void TopBarSetTitle(const cString &Title, bool Clear = true);
-        void TopBarSetTitleExtra(const cString &Extra1, const cString &Extra2);
-        void TopBarSetMenuIcon(const cString &icon);
-        void TopBarSetMenuIconRight(const cString &icon);
-        void TopBarClearMenuIconRight();
-        void TopBarSetMenuLogo(const cString &icon);
-        void TopBarSetExtraIcon(const cString &icon);
-        void TopBarUpdate();
+    void TopBarCreate();
+    void TopBarSetTitle(const cString &Title, bool Clear = true);
+    void TopBarSetTitleExtra(const cString &Extra1, const cString &Extra2);
+    void TopBarSetMenuIcon(const cString &icon);
+    void TopBarSetMenuIconRight(const cString &icon);
+    void TopBarClearMenuIconRight();
+    void TopBarSetMenuLogo(const cString &icon);
+    void TopBarSetExtraIcon(const cString &icon);
+    void TopBarUpdate();
 
-        void ButtonsCreate();
-        void ButtonsSet(const char *Red, const char *Green = nullptr, const char *Yellow = nullptr,
-                        const char *Blue = nullptr);
-        bool ButtonsDrawn() const;
+    void ButtonsCreate();
+    void ButtonsSet(const char *Red, const char *Green = nullptr, const char *Yellow = nullptr,
+                    const char *Blue = nullptr);
+    bool ButtonsDrawn() const;
 
-        void MessageCreate();
-        void MessageSet(eMessageType Type, const char *Text);
-        void MessageSetExtraTime(const char *Text);
-        void MessageClear();
+    void MessageCreate();
+    void MessageSet(eMessageType Type, const char *Text);
+    void MessageSetExtraTime(const char *Text);
+    void MessageClear();
 
-        void ProgressBarDrawRaw(cPixmap *Pixmap, cPixmap *PixmapBg, const cRect &rec, const cRect &recBg, int Current,
-                                int Total, tColor ColorFg, tColor ColorBarFg, tColor ColorBg, int Type,
-                                bool SetBackground, bool IsSignal = false);
-        void ProgressBarCreate(const cRect &Rect, int MarginHor, int MarginVer, tColor ColorFg, tColor ColorBarFg,
-                               tColor ColorBg, int Type, bool SetBackground = false, bool IsSignal = false);
-        void ProgressBarDrawBgColor() const;
-        void ProgressBarDraw(int Current, int Total);
+    void ProgressBarDrawRaw(cPixmap *Pixmap, cPixmap *PixmapBg, const cRect &rec, const cRect &recBg, int Current,
+                            int Total, tColor ColorFg, tColor ColorBarFg, tColor ColorBg, int Type,
+                            bool SetBackground, bool IsSignal = false);
+    void ProgressBarCreate(const cRect &Rect, int MarginHor, int MarginVer, tColor ColorFg, tColor ColorBarFg,
+                            tColor ColorBg, int Type, bool SetBackground = false, bool IsSignal = false);
+    void ProgressBarDrawBgColor() const;
+    void ProgressBarDraw(int Current, int Total);
 #if APIVERSNUM >= 30004
-        /**
-         * Draws marks on the progress bar with error indicators
-         * @param Errors Error markers to display (added in API v3.0.4)
-         */
-        void ProgressBarDrawMarks(int Current, int Total, const cMarks *Marks, const cErrors *Errors, tColor Color,
-                                  tColor ColorCurrent);
+    /**
+     * Draws marks on the progress bar with error indicators
+     * @param Errors Error markers to display (added in API v3.0.4)
+     */
+    void ProgressBarDrawMarks(int Current, int Total, const cMarks *Marks, const cErrors *Errors, tColor Color,
+                                tColor ColorCurrent);
 #else
-        /**
-         * Draws marks on the progress bar (legacy version)
-         * Note: Error indicators not supported in API versions before 3.0.4
-         */
-        void ProgressBarDrawMarks(int Current, int Total, const cMarks *Marks, tColor Color, tColor ColorCurrent);
+    /**
+     * Draws marks on the progress bar (legacy version)
+     * Note: Error indicators not supported in API versions before 3.0.4
+     */
+    void ProgressBarDrawMarks(int Current, int Total, const cMarks *Marks, tColor Color, tColor ColorCurrent);
 #endif
-        void ScrollbarDraw(cPixmap *Pixmap, int Left, int Top, int Height, int Total, int Offset,
-                           int Shown, bool CanScrollUp, bool CanScrollDown);
-        int ScrollBarWidth() const;
+    void ScrollbarDraw(cPixmap *Pixmap, int Left, int Top, int Height, int Total, int Offset,
+                        int Shown, bool CanScrollUp, bool CanScrollDown);
+    int ScrollBarWidth() const;
 
-        void DecorBorderDraw(const sDecorBorder &ib, bool Store = true);
-        void DecorBorderClear(const cRect &Rect, int Size);
-        void DecorBorderClearAll() const;
-        void DecorBorderRedrawAll();
-        void DecorBorderClearByFrom(int From);
+    void DecorBorderDraw(const sDecorBorder &ib, bool Store = true);
+    void DecorBorderClear(const cRect &Rect, int Size);
+    void DecorBorderClearAll() const;
+    void DecorBorderRedrawAll();
+    void DecorBorderClearByFrom(int From);
 
-        int GetFontAscender(const char *Name, int CharHeight, int CharWidth = 0) const;
+    int GetFontAscender(const char *Name, int CharHeight, int CharWidth = 0) const;
 
-        cString ReadAndExtractData(const cString &FilePath, const cString delimiter = "") const;
-        cString FormatPrecipitation(const cString &FilePath) const;
+    cString ReadAndExtractData(const cString &FilePath, const cString delimiter = "") const;
+    cString FormatPrecipitation(const cString &FilePath) const;
 
-        void DrawWidgetWeather();
-        void DrawTextWithShadow(cPixmap *pixmap, const cPoint &pos, const char *text, tColor TextColor,
-                                tColor ShadowColor, const cFont *font, int ShadowSize = 3, int xOffset = 1,
-                                int yOffset = 1);
+    void DrawWidgetWeather();
+    void DrawTextWithShadow(cPixmap *pixmap, const cPoint &pos, const char *text, tColor TextColor,
+                            tColor ShadowColor, const cFont *font, int ShadowSize = 3, int xOffset = 1,
+                            int yOffset = 1);
 
  protected:
-        cOsd *m_Osd {nullptr};
+    cOsd *m_Osd {nullptr};
 
-        int m_OsdLeft {0}, m_OsdTop {0}, m_OsdWidth {0}, m_OsdHeight {0};
-        const int m_MarginItem {5}, m_MarginItem2 {10}, m_MarginItem3 {15};
+    int m_OsdLeft {0}, m_OsdTop {0}, m_OsdWidth {0}, m_OsdHeight {0};
+    const int m_MarginItem {5}, m_MarginItem2 {10}, m_MarginItem3 {15};
 
-        static constexpr int ICON_WIDTH_UNLIMITED {999};   // Max icon width (999)
-        static constexpr int ICON_HEIGHT_UNLIMITED {999};  // Max icon height (999)
+    static constexpr int ICON_WIDTH_UNLIMITED {999};   // Max icon width (999)
+    static constexpr int ICON_HEIGHT_UNLIMITED {999};  // Max icon height (999)
 
-        // Standard fonts
-        cFont *m_Font {nullptr};
-        cFont *m_FontSml {nullptr};
-        cFont *m_FontFixed {nullptr};
-        int m_FontHeight {0}, m_FontHeight2 {0};
-        int m_FontSmlHeight {0};
-        int m_FontFixedHeight {0};
-        int m_FontAscender {0};  // Ascender for font
+    // Standard fonts
+    cFont *m_Font {nullptr};
+    cFont *m_FontSml {nullptr};
+    cFont *m_FontFixed {nullptr};
+    int m_FontHeight {0}, m_FontHeight2 {0};
+    int m_FontSmlHeight {0};
+    int m_FontFixedHeight {0};
+    int m_FontAscender {0};  // Ascender for font
 
-        cFont *m_FontBig {nullptr};      // Big font for channel name in displaychannel.c
-        cFont *m_FontMedium {nullptr};   // Font in Size between m_Font and m_FontSml
-        cFont *m_FontTempSml {nullptr};  // Font for main menu weather widget
-        cFont *m_FontTiny {nullptr};     // Very small font for actor name and role
-        int m_FontBigHeight {0};
+    cFont *m_FontBig {nullptr};      // Big font for channel name in displaychannel.c
+    cFont *m_FontMedium {nullptr};   // Font in Size between m_Font and m_FontSml
+    cFont *m_FontTempSml {nullptr};  // Font for main menu weather widget
+    cFont *m_FontTiny {nullptr};     // Very small font for actor name and role
+    int m_FontBigHeight {0};
 
-        // TopBar
-        cPixmap *TopBarPixmap {nullptr};
-        cPixmap *TopBarIconPixmap {nullptr};
-        cPixmap *TopBarIconBgPixmap {nullptr};
-        cFont *m_TopBarFont {nullptr}, *m_TopBarFontSml {nullptr}, *m_TopBarFontClock {nullptr};
-        int m_TopBarFontHeight {0}, m_TopBarFontSmlHeight {0}, m_TopBarFontClockHeight {0};
+    // TopBar
+    cPixmap *TopBarPixmap {nullptr};
+    cPixmap *TopBarIconPixmap {nullptr};
+    cPixmap *TopBarIconBgPixmap {nullptr};
+    cFont *m_TopBarFont {nullptr}, *m_TopBarFontSml {nullptr}, *m_TopBarFontClock {nullptr};
+    int m_TopBarFontHeight {0}, m_TopBarFontSmlHeight {0}, m_TopBarFontClockHeight {0};
 
-        cString m_TopBarTitle {""};
-        cString m_TopBarTitleExtra1 {""}, m_TopBarTitleExtra2 {""};
-        cString m_TopBarExtraIcon {""};
-        bool m_TopBarExtraIconSet {false};
-        cString m_TopBarMenuIcon {""};
-        bool m_TopBarMenuIconSet {false};
-        cString m_TopBarMenuIconRight {""};
-        bool m_TopBarMenuIconRightSet {false};
-        cString m_TopBarMenuLogo {""};
-        bool m_TopBarMenuLogoSet {false};
+    cString m_TopBarTitle {""};
+    cString m_TopBarTitleExtra1 {""}, m_TopBarTitleExtra2 {""};
+    cString m_TopBarExtraIcon {""};
+    bool m_TopBarExtraIconSet {false};
+    cString m_TopBarMenuIcon {""};
+    bool m_TopBarMenuIconSet {false};
+    cString m_TopBarMenuIconRight {""};
+    bool m_TopBarMenuIconRightSet {false};
+    cString m_TopBarMenuLogo {""};
+    bool m_TopBarMenuLogoSet {false};
 
-        bool m_TopBarUpdateTitle {false};
-        time_t m_TopBarLastDate {0};
-        int m_TopBarHeight {0};
-        int m_VideoDiskUsageState {-1};
+    bool m_TopBarUpdateTitle {false};
+    time_t m_TopBarLastDate {0};
+    int m_TopBarHeight {0};
+    int m_VideoDiskUsageState {-1};
 
-        // Progressbar
-        cPixmap *ProgressBarMarkerPixmap {nullptr};  // Draw marker on top
-        cPixmap *ProgressBarPixmap {nullptr};        // Draw errors here
-        cPixmap *ProgressBarPixmapBg {nullptr};
-        int m_ProgressBarHeight {0}, m_ProgressBarTop {0}, m_ProgressBarWidth {0};
-        int m_ProgressBarMarginHor {0}, m_ProgressBarMarginVer {0};
-        int m_ProgressType {0};
-        bool m_ProgressBarSetBackground {false};
-        bool m_ProgressBarIsSignal {false};
-        tColor m_ProgressBarColorFg {0}, m_ProgressBarColorBarFg {0};
-        tColor m_ProgressBarColorBarCurFg {0}, m_ProgressBarColorBg {0};
-        tColor m_ProgressBarColorMark {0}, m_ProgressBarColorMarkCurrent {0};
+    // Progressbar
+    cPixmap *ProgressBarMarkerPixmap {nullptr};  // Draw marker on top
+    cPixmap *ProgressBarPixmap {nullptr};        // Draw errors here
+    cPixmap *ProgressBarPixmapBg {nullptr};
+    int m_ProgressBarHeight {0}, m_ProgressBarTop {0}, m_ProgressBarWidth {0};
+    int m_ProgressBarMarginHor {0}, m_ProgressBarMarginVer {0};
+    int m_ProgressType {0};
+    bool m_ProgressBarSetBackground {false};
+    bool m_ProgressBarIsSignal {false};
+    tColor m_ProgressBarColorFg {0}, m_ProgressBarColorBarFg {0};
+    tColor m_ProgressBarColorBarCurFg {0}, m_ProgressBarColorBg {0};
+    tColor m_ProgressBarColorMark {0}, m_ProgressBarColorMarkCurrent {0};
 
-        // Scrollbar
-        int m_ScrollBarWidth {0};
+    // Scrollbar
+    int m_ScrollBarWidth {0};
 
-        // Buttons red, green, yellow, blue
-        cPixmap *ButtonsPixmap {nullptr};
-        int m_ButtonsWidth {0}, m_ButtonsHeight {0}, m_ButtonsTop {0};
-        const int m_MarginButtonColor {10}, m_ButtonColorHeight {8};
-        bool m_ButtonsDrawn {false};
+    // Buttons red, green, yellow, blue
+    cPixmap *ButtonsPixmap {nullptr};
+    int m_ButtonsWidth {0}, m_ButtonsHeight {0}, m_ButtonsTop {0};
+    const int m_MarginButtonColor {10}, m_ButtonColorHeight {8};
+    bool m_ButtonsDrawn {false};
 
-        // Message
-        cPixmap *MessagePixmap {nullptr}, *MessageIconPixmap {nullptr};
-        int m_MessageHeight {0};
-        cTextScrollers MessageScroller;
-        int m_OSDMessageTime {0};  // Backup for Setup.OSDMessageTime
+    // Message
+    cPixmap *MessagePixmap {nullptr}, *MessageIconPixmap {nullptr};
+    int m_MessageHeight {0};
+    cTextScrollers MessageScroller;
+    int m_OSDMessageTime {0};  // Backup for Setup.OSDMessageTime
 
-        cComplexContent WeatherWidget;
+    cComplexContent WeatherWidget;
 
-        cPixmap *DecorPixmap {nullptr};
-        std::vector<sDecorBorder> Borders;  // For clearing specific borders (Clear only 'MenuItems' and not 'TopBar')
+    cPixmap *DecorPixmap {nullptr};
+    std::vector<sDecorBorder> Borders;  // For clearing specific borders (Clear only 'MenuItems' and not 'TopBar')
 
-        void ProgressBarDrawMark(int PosMark, int PosMarkLast, int PosCurrent, bool Start, bool IsCurrent);
+    void ProgressBarDrawMark(int PosMark, int PosMarkLast, int PosCurrent, bool Start, bool IsCurrent);
 #if APIVERSNUM >= 30004
-        void ProgressBarDrawError(int Pos, int SmallLine, bool IsCurrent);
+    void ProgressBarDrawError(int Pos, int SmallLine, bool IsCurrent);
 #endif
 
-        void DecorDrawGlowRectHor(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
-        void DecorDrawGlowRectVer(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
+    void DecorDrawGlowRectHor(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
+    void DecorDrawGlowRectVer(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
 
-        void DecorDrawGlowRectTL(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
-        void DecorDrawGlowRectTR(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
-        void DecorDrawGlowRectBL(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
-        void DecorDrawGlowRectBR(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
+    void DecorDrawGlowRectTL(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
+    void DecorDrawGlowRectTR(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
+    void DecorDrawGlowRectBL(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
+    void DecorDrawGlowRectBR(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg);
 
-        void DecorDrawGlowEllipseTL(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg,
-                                    int type);
-        void DecorDrawGlowEllipseTR(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg,
-                                    int type);
-        void DecorDrawGlowEllipseBL(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg,
-                                    int type);
-        void DecorDrawGlowEllipseBR(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg,
-                                    int type);
+    void DecorDrawGlowEllipseTL(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg,
+                                int type);
+    void DecorDrawGlowEllipseTR(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg,
+                                int type);
+    void DecorDrawGlowEllipseBL(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg,
+                                int type);
+    void DecorDrawGlowEllipseBR(cPixmap *pixmap, int Left, int Top, int Width, int Height, tColor ColorBg,
+                                int type);
 
-        void TopBarEnableDiskUsage();
-        // tColor Multiply(tColor Color, uint8_t Alpha);
-        tColor SetAlpha(tColor Color, double am);
+    void TopBarEnableDiskUsage();
+    // tColor Multiply(tColor Color, uint8_t Alpha);
+    tColor SetAlpha(tColor Color, double am);
 };
 
 // Recording Timer Count: Cache with event update or periodic refresh
@@ -216,13 +216,13 @@ static std::atomic<uint16_t> s_NumRecordings {0};
 
 class RecTimerCounter {
  public:
-    std::atomic<time_t> lastUpdate;
+    std::atomic<time_t> LastUpdate;
     static constexpr int kUpdateIntervalSec = 2;
-    RecTimerCounter() : lastUpdate(0) {}
+    RecTimerCounter() : LastUpdate(0) {}
 
     void UpdateIfNeeded() {
-        time_t now = time(0);
-        if (now - lastUpdate.load() >= kUpdateIntervalSec) {
+        time_t now {time(0)};
+        if (now - LastUpdate.load() >= kUpdateIntervalSec) {
             uint16_t count {0};
             { LOCK_TIMERS_READ;
                 for (const cTimer* Timer=Timers->First(); Timer; Timer = Timers->Next(Timer)) {
@@ -230,7 +230,7 @@ class RecTimerCounter {
                 }
             }
             s_NumRecordings.store(count, std::memory_order_relaxed);
-            lastUpdate = now;
+            LastUpdate = now;
         }
     }
 };
