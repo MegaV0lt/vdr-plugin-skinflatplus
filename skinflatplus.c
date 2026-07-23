9d7f40f92f659bcafb8104401604cd551e26f468
MegaV0lt <Stefan.Blochberger@gmx.de>
2026-07-23 14:20:48 +0200

    Update History

--- skinflatplus.c
+++ skinflatplus.c
@@ -19,7 +19,7 @@
 #include "./setup.h"
 #include "./imageloader.h"
 
-static const char *VERSION     {"1.2.10"};
+static const char *VERSION     {"1.3.0"};
 static const char *DESCRIPTION {"Skin flatPlus"};
 
 class cPluginFlat : public cPlugin {
