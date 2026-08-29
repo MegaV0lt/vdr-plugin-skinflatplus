dc97b483fe196bca266a7fe5ac1050d97848ab48
MegaV0lt <M_V0lt@mail.ru>
2026-08-29 17:12:11 +0200

    Updated check_fonts_for_hairspace.sh

--- contrib/check_fonts_for_hairspace.sh
+++ contrib/check_fonts_for_hairspace.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# List fonts that have the char 'Hairspace'
+# List fonts that have the char 'Hairspace' (U+200A), almost equal to (≈) U+2248
+# and average sign (Ø) (U+00D8) in their charset.
+
+set -o nounset
 
 # /usr/share/fonts/truetype/freefont/FreeMono.ttf: FreeMono:style=Standard,нормален,normal,obyčejné,µεσαία,Regular,Normaali,Normál,Normale,Standaard,Normalny,Обычный,Normálne,menengah,прямій,navadno,vidējs,normalusis,thường,Arrunta,सामान्य
-echo -e 'Schriften, die das Zeichen "HairPace" enthalten:\n'
-mapfile -t < <(fc-list ":charset=0x200A" | sort -u)
+echo -e 'Fonts with "Hairspace", "average sign" (Ø) and "almost equal" (≈):\n'
+mapfile -t < <(fc-list ":charset=200A,2248,00D8" | sort -u)
 for font in "${MAPFILE[@]}" ; do
   : "${font%%: *}" ; file="${_##*/}"
   : "${font##*: }" ; name="${_%%,*}"
